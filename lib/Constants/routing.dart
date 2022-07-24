0245d50fda607ef522aa2f036eb3d629ddb191b0
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2022-07-25 00:28:56 +0500

    home_screen_created

---
+++ lib/Constants/routing.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:sayabi_dev/Screens/home.dart';
+
+animateRoutes(var route) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => route,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = const Offset(0.0, 1.0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return animateRoutes(const Home());
+      // case '/favorite':
+      //   return animateRoutes(Favorites());
+      // case '/orders':
+      //   return animateRoutes(Order());
+      // case '/profile':
+      //   return animateRoutes(Profile());
+      // case '/address':
+      //   return animateRoutes(Address());
+      // case '/cart':
+      //   return animateRoutes(Cart());
+      default:
+        return _errorRoute();
+    }
+  }
+}
+
+Route<dynamic> _errorRoute() {
+  return MaterialPageRoute(builder: (_) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Error"),
+      ),
+    );
+  });
+}
--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sayabi_dev/Constants/services.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,12 +9,168 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<String> cities = Services.cities;
+  String? selecteditem = 'Our Services';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text(
-        "IN",
-        style: TextStyle(color: Colors.black),
+    return SafeArea(
+      top: true,
+      bottom: true,
+      left: true,
+      right: true,
+      child: Scaffold(
+        body: Container(
+          margin:
+              EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.08),
+          padding: EdgeInsets.only(
+              left: MediaQuery.of(context).size.width * 0.06,
+              right: MediaQuery.of(context).size.width * 0.06),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Welcome",
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: MediaQuery.of(context).size.height * 0.06,
+                  color: const Color.fromRGBO(231, 27, 3, 1),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Text(
+                "Let’s Grow together",
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: MediaQuery.of(context).size.height * 0.04,
+                  color: const Color.fromRGBO(231, 27, 3, 1),
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.01,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.3,
+                child: Image.asset("assets/line.png"),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(7),
+                  color: const Color(0xffF37777),
+                ),
+                // color: Color(0xff6DC9EF),
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.1,
+                child: DropdownButtonFormField<String>(
+                  icon: Image.asset("assets/circleUp.png"),
+                  iconSize: MediaQuery.of(context).size.height * 0.1,
+                  dropdownColor: const Color(0xffFDABAB),
+                  iconEnabledColor: Colors.white,
+                  decoration: InputDecoration(
+                    // fillColor: Color(),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(7),
+                        borderSide: const BorderSide(
+                          width: 3,
+                          color: Color(0xffF37777),
+                        )),
+                  ),
+                  value: selecteditem,
+                  items: cities
+                      .map((item) => DropdownMenuItem<String>(
+                            value: item,
+                            child: Text(
+                              item,
+                              style: TextStyle(
+                                  fontSize: MediaQuery.of(context).size.height *
+                                      0.035,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Poppins'),
+                            ),
+                          ))
+                      .toList(),
+                  onChanged: (item) async {
+                    setState(() => selecteditem = item);
+                    setState(() {
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return const Center(
+                              child: CircularProgressIndicator(
+                                value: 1,
+                                color: Colors.black,
+                              ),
+                            );
+                          });
+                      selecteditem = 'Our Services';
+                    });
+                    Navigator.of(context).pushNamed('/');
+                  },
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+              ),
+              InkWell(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(7),
+                    color: const Color(0xffF37777),
+                  ),
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.1,
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(
+                            left: MediaQuery.of(context).size.width * 0.02),
+                        child: Text(
+                          " About Us",
+                          style: TextStyle(
+                              fontSize:
+                                  MediaQuery.of(context).size.height * 0.035,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ),
+                      const Spacer(),
+                      Padding(
+                        padding: EdgeInsets.only(
+                            right: MediaQuery.of(context).size.width * 0.04),
+                        child: SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.04,
+                          child: Image.asset("assets/aboutus.png"),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.08,
+          child: FittedBox(
+            child: FloatingActionButton(
+              onPressed: () {},
+              backgroundColor: const Color(0xFFF37777),
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.1,
+                width: MediaQuery.of(context).size.width * 0.08,
+                child: Image.asset("assets/chat.png"),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
