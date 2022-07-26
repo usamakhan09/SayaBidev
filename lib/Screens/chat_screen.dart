86d9dcb69f29177cf6bb409029e045ef03ba82af
Mohammad Abdur Rehman Cheema <59124007+Hami0095@users.noreply.github.com>
2022-07-26 16:46:44 +0500

    Chat_screen UI updated

--- lib/Screens/chat_screen.dart
+++ lib/Screens/chat_screen.dart
@@ -1,15 +1,170 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class ChatScreen extends StatefulWidget {
-  const ChatScreen({Key? key}) : super(key: key);
+import '../models/model_chatmessage.dart';
 
-  @override
-  State<ChatScreen> createState() => _ChatScreenState();
-}
+class ChatScreen extends StatelessWidget {
+  ChatScreen({Key? key}) : super(key: key);
 
-class _ChatScreenState extends State<ChatScreen> {
+  List<ChatMessage> messages = [
+    ChatMessage(messageContent: "Hello, sayabiDevs", messageType: "receiver"),
+    ChatMessage(messageContent: "How have you been?", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Hey randomGirl, I am doing fine. wbu?",
+        messageType: "sender"),
+    ChatMessage(messageContent: "ehhhh, doing OK.", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Is there any thing wrong?", messageType: "sender"),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Text('a');
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.05,
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.red,
+        flexibleSpace: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.only(right: 16),
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
+                ),
+                const SizedBox(
+                  width: 2,
+                ),
+                const CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://randomuser.me/api/portraits/women/8.jpg"),
+                  maxRadius: 20,
+                ),
+                const SizedBox(
+                  width: 12,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        "Random Girl",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 6,
+                      ),
+                      Text(
+                        "Online",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 11,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Stack(
+        children: [
+          ListView.builder(
+            itemCount: messages.length,
+            shrinkWrap: true,
+            padding: const EdgeInsets.only(top: 10, bottom: 10),
+            physics: const ClampingScrollPhysics(),
+            itemBuilder: (context, index) {
+              return Container(
+                padding: const EdgeInsets.only(
+                  left: 16,
+                  right: 16,
+                  top: 10,
+                  bottom: 10,
+                ),
+                child: Align(
+                  alignment: (messages[index].messageType == "receiver"
+                      ? Alignment.topLeft
+                      : Alignment.topRight),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: (messages[index].messageType == "receiver"
+                          ? Colors.grey.shade200
+                          : Colors.red[500]),
+                    ),
+                    padding: const EdgeInsets.all(16),
+                    child: Text(
+                      messages[index].messageContent,
+                      style: messages[index].messageType == "receiver"
+                          ? const TextStyle(fontSize: 15, color: Colors.black)
+                          : const TextStyle(fontSize: 15, color: Colors.white),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              padding: const EdgeInsets.only(left: 10, bottom: 10, top: 10),
+              height: 60,
+              width: double.infinity,
+              color: Colors.white,
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child:
+                          const Icon(Icons.add, color: Colors.white, size: 20),
+                    ),
+                  ),
+                  const SizedBox(width: 15),
+                  const Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Write message...",
+                        hintStyle: TextStyle(color: Colors.black54),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.send_rounded,
+                      color: Colors.white,
+                      size: 18,
+                    ),
+                    backgroundColor: Colors.red,
+                    elevation: 0,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
